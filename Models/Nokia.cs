60dcd05b423fe8c9a531a78793aadd1d19af099e
FelScheltec <146546407+FelScheltec@users.noreply.github.com>
2023-10-28 19:12:48 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,25 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+
+    class nokia : Smartphone
     {
+        public nokia (string numero, string modelo, string sistemaOperacional) : base(numero, modelo, sistemaOperacional)
+            {
+
+            }
+            public nokia (string numero, string modelo, string sistemaOperacional, int memoria) : this(numero, modelo, sistemaOperacional)
+        {
+            Memoria = memoria;
+        }
+
+        public int Memoria{get;}
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+            public override void InstalarAplicativo(string nomeApp)
+            {
+                Console.WriteLine($"Instalando o APP {nomeApp}");
+            }
+        
     }
 }
