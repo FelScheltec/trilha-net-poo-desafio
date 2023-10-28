9fb4e5f049c3b469d38be9942af5fb916e83dd06
FelScheltec <146546407+FelScheltec@users.noreply.github.com>
2023-10-28 19:14:03 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("SmartPhone Nokia: ");
+Smartphone nokia = new nokia(numero: "123", modelo: "Modelo2", sistemaOperacional: "iOS", memoria: 64 );
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("/N");
+
+
+Console.WriteLine("SmartPhone Iphone: ");
+Smartphone iphone = new iphone(numero: "12322", modelo: "Modelo12", sistemaOperacional: "IOS", memoria: 128 );
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine("/N");
+
+e
