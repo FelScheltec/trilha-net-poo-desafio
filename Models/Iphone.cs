e32f44a0544c0b1bf1f4b0a2c8e859676dd4f603
FelScheltec <146546407+FelScheltec@users.noreply.github.com>
2023-10-28 19:12:23 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,26 @@
+using System.ComponentModel;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
-    {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+
+    class iphone : Smartphone
+    { 
+            public iphone(string numero, string modelo, string sistemaOperacional) : base(numero, modelo, sistemaOperacional)
+            {
+
+            }
+
+        public iphone(string numero, string modelo, string sistemaOperacional, int memoria) : this(numero, modelo, sistemaOperacional)
+        {
+            Memoria = memoria;
+        }
+
+        public int Memoria { get; }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o APP {nomeApp}");
+        }
     }
-}
+}}
