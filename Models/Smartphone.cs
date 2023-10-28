2f14e39624d35e93b984e63f56985d86a69f8416
FelScheltec <146546407+FelScheltec@users.noreply.github.com>
2023-10-28 19:13:25 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,14 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; } // Adicione a propriedade Modelo
+        public string SistemaOperacional { get; set; } // Adicione a propriedade SistemaOperacional
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string sistemaOperacional)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo; // Passe o modelo como parâmetro para a propriedade
+            SistemaOperacional = sistemaOperacional; // Passe o sistema operacional como parâmetro para a propriedade
         }
 
         public void Ligar()
